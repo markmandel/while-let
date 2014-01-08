557964925af84e7d5549b7af66985b436004b63b
Mark Mandel <mark.mandel@gmail.com>
2014-01-08 15:48:51 +1100

    Missed the URL.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject while-let "0.1.0"
     :description "Repeatedly executes body while test expression is true, evaluating the body with binding-form bound to the value of test."
-    :url "http://example.com/FIXME"
+    :url "https://github.com/markmandel/while-let"
     :license {:name "Eclipse Public License"
               :url  "http://www.eclipse.org/legal/epl-v10.html"}
     :dependencies [[org.clojure/clojure "1.5.1"]]
