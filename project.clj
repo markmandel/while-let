1fe0f18a9206bcde1373368e0cdf355e3434b5a8
Mark Mandel <mark.mandel@gmail.com>
2015-01-27 09:09:47 +1100

    Update dependencies.

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
     :url "https://github.com/markmandel/while-let"
     :license {:name "Eclipse Public License"
               :url  "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.5.1"]]
-    :plugins [[lein-midje "3.1.1"]]
-    :profiles {:dev {:dependencies [[midje "1.6.0"]
-                                    [org.clojure/tools.namespace "0.2.4"]]
+    :dependencies [[org.clojure/clojure "1.6.0"]]
+    :plugins [[lein-midje "3.1.3"]]
+    :profiles {:dev {:dependencies [[midje "1.6.3"]
+                                    [org.clojure/tools.namespace "0.2.7"]]
                      :source-paths ["dev"]
                      :repl-options {:init-ns user}}})
