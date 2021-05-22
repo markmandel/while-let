2fc2bfe374768a565eabfe421c1f4c9b122bb151
Jim Farrand <jim.farrand-sourcecode@xyxyx.org>
2021-05-22 13:40:08 +0100

    Bring dependencies up to date
    Previous set of dependencies do not work with modern Java.

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
     :url "https://github.com/markmandel/while-let"
     :license {:name "Eclipse Public License"
               :url  "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.6.0"]]
+    :dependencies [[org.clojure/clojure "1.10.3"]]
     :plugins [[lein-midje "3.1.3"]]
-    :profiles {:dev {:dependencies [[midje "1.6.3"]
+    :profiles {:dev {:dependencies [[midje "1.10.3"]
                                     [org.clojure/tools.namespace "0.2.7"]]
                      :source-paths ["dev"]
                      :repl-options {:init-ns user}}})
